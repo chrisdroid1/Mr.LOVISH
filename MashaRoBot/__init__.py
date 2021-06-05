94df34dca17946d18dade1df6d283f9fea47e0e4
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-06-05 20:26:20 +0530

    Update __init__.py

--- MashaRoBot/__init__.py
+++ MashaRoBot/__init__.py
@@ -183,15 +183,7 @@ DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
 DEV_USERS.add(1587091205)
 
-if not SPAMWATCH_API:
-    sw = None
-    LOGGER.warning("SpamWatch API key missing! recheck your config.")
-else:
-    try:
-        sw = spamwatch.Client(SPAMWATCH_API)
-    except:
-        sw = None
-        LOGGER.warning("Can't connect to SpamWatch!")
+
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
