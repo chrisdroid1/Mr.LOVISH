6474fc3759ed83b6345d6bdb8f78fc7c8db18cde
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-06-05 20:21:56 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -746,27 +746,5 @@ if __name__ == "__main__":
     pbot.start()
     main()
     
-    ### FOR VC ##
-from pyrogram import Client as Bot
-
-from callsmusic import run
-
-from config import API_ID, API_HASH, BOT_TOKEN
-
-bot = Bot(
-
-    ":memory:",
-
-    API_ID,
-
-    API_HASH,
-
-    bot_token=BOT_TOKEN,
-
-    plugins=dict(root="handlers")
-
-)
-
-bot.start()
 
 run()
