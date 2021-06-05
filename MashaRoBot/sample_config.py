64d86a3a794774ed58c5d2a6cc5b8c5d34816676
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-06-05 18:44:55 +0530

    Update sample_config.py

--- MashaRoBot/sample_config.py
+++ MashaRoBot/sample_config.py
@@ -66,7 +66,8 @@ class Config(object):
     )
     AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
-    SPAMMERS = None
+    SPAMMERS = None 
+    SESSION_NAME = getenv("SESSION_NAME", "session")
 
 class Production(Config):
     LOGGER = True
