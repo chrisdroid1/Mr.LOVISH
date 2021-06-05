16003e6abfc65ffc4caaf3ba8e3b72e7b7fe896f
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-06-05 19:40:28 +0530

    Create callsmusic.py

---
+++ MashaRoBot/VC/callsmusic/callsmusic.py
@@ -0,0 +1,31 @@
+from pyrogram import Client
+
+from pytgcalls import PyTgCalls
+
+from sample_config import config
+
+from MashaRoBot.VC.callsmusic.queues import queues
+
+client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+
+pytgcalls = PyTgCalls(client)
+
+@pytgcalls.on_stream_end()
+
+def on_stream_end(chat_id: int) -> None:
+
+    queues.task_done(chat_id)
+
+    if queues.is_empty(chat_id):
+
+        pytgcalls.leave_group_call(chat_id)
+
+    else:
+
+        pytgcalls.change_stream(
+
+            chat_id, queues.get(chat_id)["file"]
+
+        )
+
+run = pytgcalls.run
