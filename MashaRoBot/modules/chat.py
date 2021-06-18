3101583db0faea0261070129d9b3c130026e25f6
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-06-18 12:55:59 +0530

    Create chat.py

---
+++ MashaRoBot/modules/chat.py
@@ -0,0 +1,18 @@
+__mod_name__ = "AI CHAT" 
+__help__ = f"""
+
+Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a more interactive group chat experience.
+
+*Commands:* 
+
+*Admins only:*
+
+ --> /addchat*:* Enables Chatbot mode in the chat.
+
+-->  /rmchat*:* Disables Chatbot mode in the chat.
+
+Reports bugs at @{SUPPORT_CHAT}
+
+
+
+"""
