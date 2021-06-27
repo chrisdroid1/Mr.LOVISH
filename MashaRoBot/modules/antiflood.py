34897cd77e7116aafaa8ba766f755672eb8af149
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-06-27 09:37:32 +0530

    Update antiflood.py

--- MashaRoBot/modules/antiflood.py
+++ MashaRoBot/modules/antiflood.py
@@ -419,7 +419,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "Floods🌊"
+__mod_name__ = "ғʟᴏᴏᴅs🌊"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood
