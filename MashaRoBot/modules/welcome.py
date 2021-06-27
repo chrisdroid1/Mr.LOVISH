781a648e7cb93f15335d4226d33ab931472f2d0e
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-06-27 09:57:13 +0530

    Update welcome.py

--- MashaRoBot/modules/welcome.py
+++ MashaRoBot/modules/welcome.py
@@ -1144,7 +1144,7 @@ dispatcher.add_handler(CLEAN_SERVICE_HANDLER)
 dispatcher.add_handler(BUTTON_VERIFY_HANDLER)
 dispatcher.add_handler(WELCOME_MUTE_HELP)
 
-__mod_name__ = "WELCOMES🤗"
+__mod_name__ = "ᴡᴇʟᴄᴏᴍᴇ🤗"
 __command_list__ = []
 __handlers__ = [
     NEW_MEM_HANDLER,
