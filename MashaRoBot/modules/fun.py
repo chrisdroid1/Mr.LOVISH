d84f013d523e7f08265939e297dcf0725bf91e43
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-06-27 09:42:07 +0530

    Update fun.py

--- MashaRoBot/modules/fun.py
+++ MashaRoBot/modules/fun.py
@@ -371,7 +371,7 @@ dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(EIGHTBALL_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
 
-__mod_name__ = "MEMES🎭"
+__mod_name__ = "ᴍᴇᴍᴇs😹"
 __command_list__ = [
     "runs",
     "slap",
