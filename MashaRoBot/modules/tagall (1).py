6fd6c62571366e46d7a34d3b460007ef61c24ccc
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-06-27 10:15:37 +0530

    Update tagall (1).py

--- MashaRoBot/modules/tagall (1).py
+++ MashaRoBot/modules/tagall (1).py
@@ -22,7 +22,7 @@ async def tagall(client, message):
         await client.send_message(message.chat.id, j, parse_mode="html")
 
 
-__mod_name__ = "ᴛᴀɢᴀʟʟ👁👀"
+__mod_name__ = "ᴛᴀɢᴀʟʟ👀"
 __help__ = """
 - /tagall : Tag everyone in a chat group
 """
