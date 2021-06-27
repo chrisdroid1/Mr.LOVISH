dc48221948219fe79a7e7be2aaf6966e36f47aa7
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-06-27 10:13:27 +0530

    Update video.py

--- MashaRoBot/modules/video.py
+++ MashaRoBot/modules/video.py
@@ -5,3 +5,12 @@ __help__ = """
 - /video : get Video from YT link
 
 """
+__mod_name__ = "ᴛʀᴀᴄᴋᴇʀ📶"
+
+__help__ = """
+
+- /phone : Track phone no. of anyone Use International format 
+
+~ @Psycho_Bots
+
+"""
