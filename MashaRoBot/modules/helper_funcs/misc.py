b8995f22ebe386ccc408b5fc514ec385ffc332ab
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-07-01 11:35:58 +0530

    Update misc.py

--- MashaRoBot/modules/helper_funcs/misc.py
+++ MashaRoBot/modules/helper_funcs/misc.py
@@ -62,9 +62,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             ]
         )
 
-    pairs = [modules[i * 5 : (i + 1) * 5] for i in range((len(modules) + 5 - 1) // 5)]
+    pairs = [modules[i * 4 : (i + 1) * 4] for i in range((len(modules) + 4 - 1) // 4)]
 
-    round_num = len(modules) / 5
+    round_num = len(modules) / 4
     calc = len(modules) - round(round_num)
     if calc in [1, 2]:
         pairs.append((modules[-1],))
