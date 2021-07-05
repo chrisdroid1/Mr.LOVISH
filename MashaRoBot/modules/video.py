f072b219b3ddd65231bb49817031b05b64e31e25
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-07-05 15:12:34 +0530

    Update video.py

--- MashaRoBot/modules/video.py
+++ MashaRoBot/modules/video.py
@@ -2,6 +2,7 @@ __mod_name__ = "ᴠɪᴅᴇᴏ🎥"
 
 __help__ = """
 - /video - GET VIDEO AT BEST QUALITY FROM YouTube
-- /lkvideo - Download video from Youtube link
+- /lkvideo - Download video from Youtube link 
+
 """
 
